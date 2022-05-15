f8c04c72e9a51298d8a65a3d15abb2e039b1a2aa
wdmcdonough <wdmcdonough@gmail.com>
2022-05-14 21:37:47 -0500

    sign up page and user controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "chatroom#index"
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
 end
